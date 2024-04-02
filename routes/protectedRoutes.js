18350364538e6b6e0041eff964c46b1d5f2bcce7
4acle <cjnarey@gmail.com>
2024-04-02 15:23:05 -0400

    Fix missing export in protectedRoutes

--- routes/protectedRoutes.js
+++ routes/protectedRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const requireAuth = require('../middleware/requireAuth');
-
 const router = express.Router();
 
 router.get('/protected', requireAuth, (req, res) => {
   res.send("This route is protected.");
 });
+
+module.exports = router;
